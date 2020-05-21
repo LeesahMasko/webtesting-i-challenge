569745d63d6e65afc480eff9733f4fdeaa84e05d
Lisa Maskovich <lisamariejensenmaskovich@gmail.com>
2020-05-20 18:36:54 -0700

    holy cow, I am done with mvp! And it's not even 7pm

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,30 +6,61 @@ module.exports = {
 };
 
 function succeed(item) {
+  let enhancement = item.enhancement;
   if (item.enhancement < 20 && item.enhancement > 0) {
-    item.enhancement++
+    enhancement++
   }
-  return { ...item };
+  return { ...item, enhancement };
 }
 
 function fail(item) {
-  if (item.enhancment < 15) {
-    item.durability = item.durability - 5
+  let enhancement = item.enhancement;
+  let durability = item.durability;
 
-  } else {
-    if (item.enhancement >= 15) {
-      item.durability = item.durability - 10
-    } else {
-      if (item.enhancement > 16) {
-        item.enhancement = item.enhancement - 1
-      }
-      return { ...item };
+  if (item.enhancement >= 15) {
+    durability -= 10;
+    if (item.enhancement > 16) {
+      enhancement--;
     }
+  } else  {
+    durability -=5
+  }
+  if (durability < 0) {
+    durability = 0;
+  }
+  return {
+    ...item,
+    durability,
+    enhancement,
+  }
+}
+
+// function fail(item) {
+//   let durability = item.durability
+//   if (item.enhancment < 15) {
+//     durability = durability - 5
+
+//   } else {
+//     if (item.enhancement >= 15) {
+//       item.durability = item.durability - 10
+//     } else {
+//       if (item.enhancement > 16) {
+//         item.enhancement = item.enhancement - 1
+//       }
+
+//     }
+//   }
+//   return { ...item, durability:durability };
+// }
 
 
 function repair(item) {
-      return { ...item, durability: 100 };
-    }
+  return { ...item, durability: 100 };
+}
+
+function get(item) {
+  return { ...item };
+}
 
     // function get(item) {
     //   return { ...item };
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,14 +1,51 @@
 const enhancer = require('./enhancer.js');
 // test away!
 
-test("suceed", () => {
-  expect(enhancer.suceed({enhancement: 17})).toEqual({enhancement: 18})
+// const testItem1= {
+//   name: 'store bought cookies',
+//   durability: 50,
+//   enhancement: 15
+// }
+// const testItem2= {
+//   name: 'homemade cookies',
+//   durability: 75,
+//   enhancement: 15
+// }
+// const testItem3= {
+//   name: 'hot homemade cookies with icecream',
+//   durability: 85,
+//   enhancement: 19
+// }
+
+describe('succeed', () => {
+  test("suceed", () => {
+    expect(enhancer.succeed({ enhancement: 5 })).toEqual({enhancement: 6})
+  })
 })
 
-test("fail", () => {
 
-})
 
 test("repair", () => {
   expect(enhancer.repair({durability:50})).toEqual({durability:100})
+})
+
+
+
+describe("fail", () => {
+  test("durability should decrease by 5 when enhancement is less than 15", () => {
+    expect(enhancer.fail({enhancement:14, durability:45})).toEqual({enhancement: 14, durability:40})
+    expect(enhancer.fail({enhancement:14, durability:0})).toEqual({enhancement: 14, durability:0})
+  })
+
+  test("durability should decrease by 10 when enhancement is 15 or more", () => {
+    expect(enhancer.fail({enhancement:15, durability:20})).toEqual({enhancement:15, durability:10})
+  })
+
+
+  test("enhancement should decrease by 1 when the enhancement level is greater than 16", () => {
+    expect(enhancer.fail({enhancement:17, durability:20})).toEqual({enhancement:16, durability:10})
+
+
+
+  })
 })
